26812a2ca8137fac51dda15466d1339d7e5be9ef
Zaki Sediqyar <zaki.sediqyar@gmail.com>
2018-09-05 06:28:57 +0000

    scroll amount changed in js file

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,5 @@
 // Navbar Scroll Handler
 
     $(window).scroll(function () {
-        $("nav").toggleClass("scrolled", $(this).scrollTop () > 800);
+        $("nav").toggleClass("scrolled", $(this).scrollTop () > 200);
     });
