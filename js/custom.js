916d691ea8c3822041eecf69752655fb32b4ed00
Zaki Sediqyar <zaki.sediqyar@gmail.com>
2018-09-06 06:26:30 +0000

    ...

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,13 @@
 // Navbar Scroll Handler
 
     $(window).scroll(function () {
-        $("nav").toggleClass("scrolled", $(this).scrollTop () > 200);
+        $("nav").toggleClass("scrolled", $(this).scrollTop () > 50);
     });
+    
+    $(window).ready( function () {
+        if ($(window).width() < 514) {
+            $("html").removeClass("fixed-top");
+        }
+    });
+    
+  
