fc9ad2df718feb535f5a25e8664372d71dac70ad
Zaki Sediqyar <zaki.sediqyar@gmail.com>
2018-09-20 06:24:30 +0000

    cleaned the code, change price cards, navbar scroll effect changed

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,13 @@
 /* global $ */
-
 // Navbar Scroll Handler
 
     $(window).scroll(function () {
         $("nav").toggleClass("scrolled", $(this).scrollTop () > 50);
+        $(".nav-link").toggleClass("scrolled", $(this).scrollTop () > 50);
     });
-    
+
     $(window).ready( function () {
         if ($(window).width() < 514) {
             $("html").removeClass("fixed-top");
         }
     });
-    
-  
